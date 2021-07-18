bd031e7cad81713d9654a12d91fc87fcbb55a004
khaidemsandipsingha <khaidemsandipsingha@gmail.com>
2021-07-18 22:02:32 +0530

    Canvas commit

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let music_name = "https://raw.githubusercontent.com/Xiija/TestFiles/master/Yuki%20%26%20Tako%2001.mp3";
+let music_name = "https://raw.githubusercontent.com/Xiija/TestFiles/master/Yuki%20%26%20Tako%2001.mp3"
 let play_btn = document.querySelector("#play");
 let range = document.querySelector("#range");
 let total_time = 0;
@@ -7,55 +7,36 @@ let isPlaying = false;
 let addCol = true
 let song = new Audio();
 var handle
-let addColor = document.getElementById("added")
+let canvas = document.querySelector('canvas')
+var c = canvas.getContext('2d')
+var val = 2
 
 
 function playSong() {
     song.src = music_name;
-
-    // Column add to graph
     function add() {
-        addColor.innerHTML += `
-                <div style="width: 10px; height: ${Math.floor(range.value - Math.random() * 30)}px;background: blue;">      
-                </div>
-            `
+        val = range.value
+        c.fillRect(val, 50, 5, Math.floor((Math.random() - 0.5) * 70))
     }
 
-    // Play the song on clicked when button displays as Play
-    // Pause the song on clicked when button displays as Pause
     play_btn.addEventListener('click', function () {
         if (!isPlaying) {
-
             song.play();
             isPlaying = true;
+            canvas.height = song.duration
+            canvas.width = song.duration
             total_time = song.duration;
             range.max = total_time;
-
-            // onClick on column graph, pause the song 
-            addColor.addEventListener('click', function () {
-                song.pause();
-                document.getElementById("play_img").innerHTML = "PLAY";
-                clearInterval(handle);
-            })
-
-            handle = setInterval(add, 3000)
-
+            handle = setInterval(add, 1000)
             document.getElementById("play_img").innerHTML = "PAUSE";
         } else {
-            addColor.addEventListener('click', function () {
-                song.play();
-                handle = setInterval(add, 3000)
-                document.getElementById("play_img").innerHTML = "PAUSE";
-            })
             clearInterval(handle)
             song.pause();
             isPlaying = false;
             document.getElementById("play_img").innerHTML = "PLAY";
         }
 
-        // clear the column graph when song ends
         song.addEventListener('ended', function () {
-            addColor.innerHTML = ""
             clearInterval(handle);
             song.currentTime = 0
             song.pause();
@@ -63,16 +44,15 @@ function playSong() {
             range.value = 0;
             document.getElementById("play_img").innerHTML = "PLAY";
         })
-
-        // Update the song timer to the current range value
         song.addEventListener('timeupdate', function () {
             range.value = song.currentTime;
         })
-
         range.addEventListener('change', function () {
             song.currentTime = range.value;
         })
+
     })
+
 }
 
 playSong()
